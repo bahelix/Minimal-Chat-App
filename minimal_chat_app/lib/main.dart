a6dd8d1a394caed6362d7a1ad3c800604d4a910e
bahelix <iglesias.xvi@gmail.com>
2024-08-13 12:14:23 +0200

    Connect Firebase

--- minimal_chat_app/lib/main.dart
+++ minimal_chat_app/lib/main.dart
@@ -1,13 +1,17 @@
+import 'package:minimal_chat_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:minimal_chat_app/auth/login_or_register.dart';
 import 'package:minimal_chat_app/themes/light_mode.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
